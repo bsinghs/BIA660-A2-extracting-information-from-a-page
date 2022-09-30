28ae29cc53f1c93b7018b1e15b6d20d9f22f0fd1
Bhajanpreet Singh <bhajanpreets@gmail.com>
2022-09-30 15:49:44 -0400

    code synchup

--- tld.py
+++ tld.py
@@ -1,15 +1,24 @@
 # Scope is to find Top Level Domains ffrom wiki page
 import requests
-import parser
+from urllib.parse import urlparse
+
 from bs4 import BeautifulSoup
 
 url = "https://en.m.wikipedia.org/wiki/List_of_Internet_top-level_domainsLinks"
 
 response = requests.get(url=url)
 tld_soup = BeautifulSoup(response.content, "html.parser")
-links = tld_soup.find_all('a')
 
-for link in links:
-    href = link.get("href")
-    print(href)
+# links = tld_soup.find_all('a')
+# for link in links:
+#     href = link.get("href")
+#     print(href)
+#     if urlparse(href).hostname is not None:
+#         print(href)
 
+tables = tld_soup.find_all('table')
+for table in tables:
+    tr = table.find_all('tr')
+    print(dir(tr))
+    tdlist = tr.find_all('td')
+    print(tr.pop)
